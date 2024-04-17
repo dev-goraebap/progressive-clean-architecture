dfe85b0fafbbd42a42ca77e5bd0092bd37dd070d
고래밥 <dev.goraebap@gmail.com>
2024-04-17 20:57:42 +0900

    setting: 파이어베이스 모듈 설정

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
 
+import { environment } from 'src/shared/environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+    ])
+  ]
 };
