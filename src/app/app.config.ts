03ff24e6b3483976c35caec4c36220d790eef3c8
고래밥 <dev.goraebap@gmail.com>
2024-04-17 21:01:36 +0900

    setting: httpClient 모듈 등록

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { environment } from 'src/shared/environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     importProvidersFrom([
       provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
       provideFirestore(() => getFirestore()),
