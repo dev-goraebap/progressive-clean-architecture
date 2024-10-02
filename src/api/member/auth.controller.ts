991eaabc8d20184380745929078dbe86a9c86d22
dev.goraebap <dev.goraebap@gmail.com>
2024-10-02 21:36:55 +0900

    Update: 인증컨트롤러 스웨거 데코레이터 적용

--- src/api/member/auth.controller.ts
+++ src/api/member/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { LocalAuthService } from "src/app/member";
 
 @Controller({ path: 'auth', version: '1' })
+@ApiTags('사용자인증')
 export class AuthController {
 
     constructor(
@@ -9,7 +11,8 @@ export class AuthController {
     ) { }
 
     @Post()
-    async register() { 
+    @ApiOperation({ summary: '일반회원가입' })
+    async register() {
         return await this.localAuthService.register('');
     }
 }
