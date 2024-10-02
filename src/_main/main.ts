2a57a15f847047fec1e45b19f97686437c507dd6
dev.goraebap <dev.goraebap@gmail.com>
2024-10-02 21:35:27 +0900

    Update: 스웨거 설정

--- src/_main/main.ts
+++ src/_main/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   app.enableVersioning();
+
+  const config = new DocumentBuilder()
+    .setTitle('PCA')
+    .setDescription('progressive clean architecture')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
