bc008a9a017eb3ca3271efc84fb9a884e30890ad
dev.goraebap <dev.goraebap@gmail.com>
2024-10-10 20:52:16 +0900

    Update: 파이어베이스 모듈 추가

---
+++ src/shared/firebase/firebase.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as admin from 'firebase-admin';
+import { EnvConfig } from "../config";
+
+@Injectable()
+export class FirebaseService {
+
+    private readonly logger = new Logger(FirebaseService.name);
+
+    constructor(
+        private readonly configService: ConfigService<EnvConfig>
+    ) {
+        try {
+            admin.initializeApp({
+                credential: admin.credential.cert({
+                    projectId: this.configService.get<string>('FIREBASE_PROJECT_ID'),
+                    privateKey: this.configService.get<string>('FIREBASE_PRIVATE_KEY'),
+                    clientEmail: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+                }),
+            });
+            this.logger.log('Firebase Admin SDK initialized successfully');
+        } catch (error) {
+            this.logger.error('Failed to initialize Firebase Admin SDK', error);
+        }
+    }
+}
